1cd5204fc187f880b9102f9a62ed1ad78d42dbd7
Alessio <76960572+VoidTechAddict@users.noreply.github.com>
2023-01-10 15:21:49 +0100

    Note
    I've added the version that write the result inside a .txt file on C:\

---
+++ HASHCheck.ps1
@@ -0,0 +1,31 @@
+﻿# Richiedi la directory del file
+$hahs1 = Read-Host -Prompt 'Direcotry File 1'
+$hahs2 = Read-Host -Prompt 'Direcotry File 2'
+
+# Ottieni gli hash dai file
+$hashsourcefile = Get-FileHash $hahs1 -Algorithm SHA256 | Select-Object -ExpandProperty Hash
+$hashdestfile = Get-FileHash $hahs2 -Algorithm SHA256 | Select-Object -ExpandProperty Hash
+
+# Compara gli hash
+if ($hashsourcefile -eq $hashdestfile) {
+
+Write-Host "Gli hash:
+
+$hashsourcefile
+$hashdestfile 
+
+Risultano uguali,il file non è stato modificato" -ForegroundColor Green
+
+
+
+} else {
+Write-Host "Gli hash:
+
+$hashsourcefile
+$hashdestfile 
+
+Risultano differenti,il file è stato modificato" -ForegroundColor Red
+
+}
+
+pause
